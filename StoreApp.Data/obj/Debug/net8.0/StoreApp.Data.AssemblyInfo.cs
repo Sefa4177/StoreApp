88fcc3f557fedf25a88ba852f0a113280006625f
Safa Can Pehlivan <88760711+Sefa4177@users.noreply.github.com>
2024-01-09 09:54:12 +0300

    Model + Controller
    Model ve Controllerdaki işlemler yapılmaya başlandı

--- StoreApp.Data/obj/Debug/net8.0/StoreApp.Data.AssemblyInfo.cs
+++ StoreApp.Data/obj/Debug/net8.0/StoreApp.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec43494fe69ef2395273a07c55d3a3fbf6838600")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- StoreApp.Web/Controllers/HomeController.cs
+++ StoreApp.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using StoreApp.Data.Abstract;
+using StoreApp.Web.Models;
 
 namespace StoreApp.Web.Controllers;
 
@@ -11,5 +12,19 @@ public class HomeController:Controller
     {
         _storeRepository = storeRepository;
     }
-    public IActionResult Index() => View();
+    public IActionResult Index()
+    {   
+        var products = _storeRepository.Products.Select(p => new ProductViewModel{
+            Id = p.Id,
+            Name = p.Name,
+            Description = p.Description,
+            Price = p.Price,
+            Category = p.Category
+
+        }).ToList();
+
+        return View(new ProductListViewModel {
+            Products = products
+        });
+    }
 }
